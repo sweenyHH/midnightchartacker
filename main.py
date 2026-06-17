69a758b0bb811e4ebf952313ef5f11248cabec7f
Sweeny_HH <malte.maehlmann@ancrepharecanaris.fr>
2026-06-17 12:28:52 +0200

    added the main.py v1

---
+++ main.py
@@ -0,0 +1,23 @@
+
+# Entry point of the application. Initializes the Qt application, starts the main window.
+
+
+import sys
+from PySide6.QtWidgets import QApplication
+from app.ui.main_window import MainWindow
+
+
+def main():
+# Create the Qt application instance
+    app = QApplication(sys.argv)
+
+# Create and show main window
+    window = MainWindow()
+    window.show()
+
+# Start application event loop
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
